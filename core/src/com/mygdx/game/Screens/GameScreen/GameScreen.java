eb3fe608c24d4465cc4226fa9ea434ec14131b39
Akp132 <109806976+Akp132@users.noreply.github.com>
2022-12-06 01:18:11 +0530

    commit by akshay

--- core/src/com/mygdx/game/Screens/GameScreen/GameScreen.java
+++ core/src/com/mygdx/game/Screens/GameScreen/GameScreen.java
@@ -1,15 +1,18 @@
 package com.mygdx.game.Screens.GameScreen;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.AddListenerAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -17,6 +20,7 @@ import com.mygdx.game.AP_Game;
 import com.mygdx.game.Screens.player1.AtomicTank1;
 import com.mygdx.game.Screens.player1.Mark1Tank1;
 import com.mygdx.game.Screens.player1.ToxicTank1;
+import com.badlogic.gdx.InputProcessor;
 
 import java.util.Objects;
 
@@ -44,6 +48,15 @@ public class GameScreen implements Screen {
 
     private Image player1TankImage;
     private Image player2TankImage;
+
+    private Vector2 movement = new Vector2();
+    private Vector2 movement2 = new Vector2();
+    private float speed = 25000;
+
+    private Body player1TankBody;
+    private Body player2TankBody;
+
+
 
 
 
@@ -83,17 +96,108 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-    world = new World(new Vector2(0, -9.8f), true);
+    world = new World(new Vector2(0, -200f), false);
     debugRenderer = new Box2DDebugRenderer();
     camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-    //BODY DEFINITION
+
+    Gdx.input.setInputProcessor((new InputController() {
+                @Override
+                public boolean keyDown(int keycode) {
+                    switch (keycode) {
+                        case Input.Keys.W:
+                            movement.y = speed;
+                            break;
+                        case Input.Keys.UP:
+                            movement2.y = speed;
+                            break;
+                        case Input.Keys.A:
+                            movement.x = -speed;
+                            break;
+                        case Input.Keys.LEFT:
+                            movement2.x = -speed;
+                            break;
+                        case Input.Keys.S:
+                            movement.y = -speed;
+                            break;
+                        case Input.Keys.DOWN:
+                            movement2.y = -speed;
+                            break;
+                        case Input.Keys.D:
+                            movement.x = speed;
+                            break;
+                        case Input.Keys.L:
+                            movement2.x = speed;
+                            break;
+
+                    }
+                    return true;
+                }
+
+                @Override
+                public boolean keyUp(int keycode) {
+                    switch (keycode) {
+                        case Input.Keys.W:
+                            movement.y = 0;
+                            break;
+                        case Input.Keys.A:
+                            movement.x = 0;
+                            break;
+                        case Input.Keys.S:
+                            movement.y = 0;
+                            break;
+                        case Input.Keys.D:
+                            movement.x = 0;
+                            break;
+                    }
+                    return true;
+                }
+
+
+            }));
+
+
+
+
+        //Circle body
+
+        BodyDef balldef1 = new BodyDef();
+        balldef1.type = BodyDef.BodyType.DynamicBody;
+
+        BodyDef balldef2 = new BodyDef();
+        balldef2.type = BodyDef.BodyType.DynamicBody;
+
+        //ball shape
+
+        CircleShape ballshape = new CircleShape();
+        ballshape.setRadius(10);
+
+
+    //fixture def
+
+        FixtureDef Player1 = new FixtureDef();
+        Player1.shape = ballshape;
+        Player1.density = 2.5f;
+        Player1.friction = 0.70f;
+        Player1.restitution = 0.75f;
+        balldef1.position.set(-50, 100);
+
+        FixtureDef Player2 = new FixtureDef();
+        Player2.shape = ballshape;
+        Player2.density = 2.5f;
+        Player2.friction = 0.70f;
+        Player2.restitution = 0.75f;
+        balldef2.position.set(50, 100);
+
+
+
+    //Ground DEFINITION
     bodyDef.type = BodyDef.BodyType.StaticBody;
-    bodyDef.position.set(0, 0);
+    bodyDef.position.set(0, 1);
     groundshape = new ChainShape();
     groundshape.createChain(new Vector2[]{
-            new Vector2(-500, -100),
-            new Vector2(500, -100),
+            new Vector2(-10000, -100),
+            new Vector2(10000, -100),
 
         });
 
@@ -104,8 +208,14 @@ public class GameScreen implements Screen {
         fixtureDef.restitution = 0;
 
 
+        //CREATION
+        player1TankBody = world.createBody(balldef1);
+        player1TankBody.createFixture(Player1);
+        player2TankBody = world.createBody(balldef2);
+        player2TankBody.createFixture(Player2);
         world.createBody(bodyDef).createFixture(fixtureDef);
         groundshape.dispose();
+        ballshape.dispose();
 
 
     }
@@ -119,7 +229,10 @@ public class GameScreen implements Screen {
 
         stage.draw();
         debugRenderer.render(world, camera.combined);
-        world.step(1/60f, 6, 2);
+        world.step(1/60f, 8, 3);
+        player1TankBody.applyForceToCenter(movement, true);
+        //player2TankBody.applyForceToCenter(movement2, true);
+
         game.batch.begin();
         game.batch.end();
 
